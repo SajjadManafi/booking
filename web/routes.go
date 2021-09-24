6d8855280c3baf7f29872f063b659be6861d05e2
SajjdManafi <sajadmanafi8@gmail.com>
2021-09-24 17:32:02 +0330

    enabling static files

--- web/routes.go
+++ web/routes.go
@@ -19,5 +19,8 @@ func routes(app *config.AppConfig) http.Handler {
 	mux.Get("/", handler.Repo.Home)
 	mux.Get("/About", handler.Repo.About)
 	mux.Get("/about", handler.Repo.About)
+
+	fileServer := http.FileServer(http.Dir("../static/"))
+	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
 	return mux
 }

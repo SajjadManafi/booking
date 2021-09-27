c9c08ffcb858f55000dcf5672028da49b3d8bd5e
SajjdManafi <sajadmanafi8@gmail.com>
2021-09-27 21:40:19 +0330

    writing test for POST handlers

--- internal/handler/handlers.go
+++ internal/handler/handlers.go
@@ -159,7 +159,6 @@ func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
 func (m *Repository) ReservationSummery(w http.ResponseWriter, r *http.Request) {
 	resetvation, ok := m.App.Session.Get(r.Context(), "reservation").(models.Reservation)
 	if !ok {
-		log.Println("cannot get item from session")
 		m.App.Session.Put(r.Context(), "error", "Can't get reservation from session")
 		http.Redirect(w,r,"/",http.StatusTemporaryRedirect)
 		return
--- internal/handler/handlers_test.go
+++ internal/handler/handlers_test.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"net/http"
 	"net/http/httptest"
+	"net/url"
 	"testing"
 )
 
@@ -10,23 +11,36 @@ type postData struct {
 	key, value string
 }
 
-var theTest =[]struct{
-	name string
-	url string
-	method string
-	params []postData
+var theTest = []struct {
+	name               string
+	url                string
+	method             string
+	params             []postData
 	expectedStatusCode int
 }{
-	{"home", "/","GET",[]postData{}, http.StatusOK},
-	{"about", "/about","GET",[]postData{}, http.StatusOK},
-	{"gq", "/generals-quarters","GET",[]postData{}, http.StatusOK},
-	{"ms", "/majors-suite","GET",[]postData{}, http.StatusOK},
-	{"sa", "/search-availability","GET",[]postData{}, http.StatusOK},
-	{"contact", "/contact","GET",[]postData{}, http.StatusOK},
-	{"mr", "/make-reservation","GET",[]postData{}, http.StatusOK},
-	{"rs", "/reservation-summery","GET",[]postData{}, http.StatusOK},
+	{"home", "/", "GET", []postData{}, http.StatusOK},
+	{"about", "/about", "GET", []postData{}, http.StatusOK},
+	{"gq", "/generals-quarters", "GET", []postData{}, http.StatusOK},
+	{"ms", "/majors-suite", "GET", []postData{}, http.StatusOK},
+	{"sa", "/search-availability", "GET", []postData{}, http.StatusOK},
+	{"contact", "/contact", "GET", []postData{}, http.StatusOK},
+	{"mr", "/make-reservation", "GET", []postData{}, http.StatusOK},
+	{"rs", "/reservation-summery", "GET", []postData{}, http.StatusOK},
+	{"post-search-avail", "/search-availability", "POST", []postData{
+		{key: "start-date", value: "2020-01-01"},
+		{key: "end-date", value: "2020-01-02"},
+	}, http.StatusOK},
+	{"post-search-avail-json", "/search-availability-json", "POST", []postData{
+		{key: "start-date", value: "2020-01-01"},
+		{key: "end-date", value: "2020-01-02"},
+	}, http.StatusOK},
+	{"post-make-reservation", "/make-reservation", "POST", []postData{
+		{key: "first_name", value: "Harry"},
+		{key: "last_name", value: "Potter"},
+		{key: "phone_number", value: "50505-66-42"},
+		{key: "email", value: "Harry@magic.com"},
+	}, http.StatusOK},
 }
-
 
 func TestHandlers(t *testing.T) {
 
@@ -34,9 +48,9 @@ func TestHandlers(t *testing.T) {
 	ts := httptest.NewTLSServer(routes)
 	defer ts.Close()
 
-	for _,e := range theTest{
+	for _, e := range theTest {
 		if e.method == "GET" {
-			resp, err:= ts.Client().Get(ts.URL+ e.url)
+			resp, err := ts.Client().Get(ts.URL + e.url)
 			if err != nil {
 				t.Log(err)
 				t.Fatal(err)
@@ -45,8 +59,20 @@ func TestHandlers(t *testing.T) {
 			if resp.StatusCode != e.expectedStatusCode {
 				t.Errorf("for %s, expected %d but got %d", e.name, e.expectedStatusCode, resp.StatusCode)
 			}
-		} else {}
+		} else {
+			values := url.Values{}
+			for _, x := range e.params {
+				values.Add(x.key, x.value)
+			}
+			resp, err := ts.Client().PostForm(ts.URL+e.url, values)
+			if err != nil {
+				t.Log(err)
+				t.Fatal(err)
+			}
+
+			if resp.StatusCode != e.expectedStatusCode {
+				t.Errorf("for %s, expected %d but got %d", e.name, e.expectedStatusCode, resp.StatusCode)
+			}
+		}
 	}
 }
-
-
